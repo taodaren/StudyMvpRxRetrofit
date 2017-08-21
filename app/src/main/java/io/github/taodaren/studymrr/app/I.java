491b6c0c2c91c9c97a2f824c910f4b3bcb473f70
taodaren <taodaren1125@gmail.com>
2017-08-21 17:23:49 +0800

    添加全局接口参数 I 接口

---
+++ app/src/main/java/io/github/taodaren/studymrr/app/I.java
@@ -0,0 +1,12 @@
+package io.github.taodaren.studymrr.app;
+
+/**
+ * 全局接口参数
+ */
+
+public interface I {
+    //基础 url
+    String BASE_URL = "https://api.douban.com/v2/";
+    //拼接 url
+    String ADD_BOOK_SEARCH = "book/search";
+}
