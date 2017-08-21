a80d02d76317ee4ca3b29de1805b45288ac9c221
taodaren <taodaren1125@gmail.com>
2017-08-21 17:36:45 +0800

    创建 View 层接口类

---
+++ app/src/main/java/io/github/taodaren/studymrr/service/view/BookView.java
@@ -0,0 +1,15 @@
+package io.github.taodaren.studymrr.service.view;
+
+import io.github.taodaren.studymrr.service.bean.BookBean;
+
+/**
+ * 展示 book 接口
+ */
+
+public interface BookView extends View {
+    //如果 presenter 请求成功，将向该方法传入请求下来的实体类
+    void onSuccess(BookBean bookBean);
+
+    //如果请求失败，就会向这个 view 传入失败信息
+    void onError(String result);
+}
---
+++ app/src/main/java/io/github/taodaren/studymrr/service/view/View.java
@@ -0,0 +1,8 @@
+package io.github.taodaren.studymrr.service.view;
+
+/**
+ * 用于区别 Android 中的 View
+ */
+
+public interface View {
+}
